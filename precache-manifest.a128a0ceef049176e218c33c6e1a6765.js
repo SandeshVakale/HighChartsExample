7e8642c02c1a352406d15fee61b118f6ee6ab2fe
Sandesh VAKALE <sandyrv.vakale@gmail.com>
2020-06-03 17:36:17 +0200

    Updates

---
+++ precache-manifest.a128a0ceef049176e218c33c6e1a6765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7071b204189df4d810868fca1874e06",
+    "url": "/HighChartsExample/index.html"
+  },
+  {
+    "revision": "aa7e6f3c02ad29a910ee",
+    "url": "/HighChartsExample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8b35c27f72650d67396",
+    "url": "/HighChartsExample/static/js/2.05396b50.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HighChartsExample/static/js/2.05396b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7e6f3c02ad29a910ee",
+    "url": "/HighChartsExample/static/js/main.69e0fc20.chunk.js"
+  },
+  {
+    "revision": "ddcb684a39379228a100",
+    "url": "/HighChartsExample/static/js/runtime-main.742297da.js"
+  }
+]);
